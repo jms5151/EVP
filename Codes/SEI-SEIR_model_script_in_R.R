1bd56877cbc2f9a09dccee4695270047ae2e2e32
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2018-12-03 13:43:52 -0800

    Initial commit for rainfall only model.

--- Codes/SEI-SEIR_model_script_in_R.R
+++ Codes/SEI-SEIR_model_script_in_R.R
@@ -11,10 +11,10 @@ seiseir_model_t <- function(t, state, parameters) {
     dM1 <- (EFD(temp[t])*pEA(temp[t])*MDR(temp[t])*mu_t(temp[t])^(-1))*(M1+M2+M3)*max((1-((M1+M2+M3)/K_t(temp[t]))),0)-(a(temp[t])*pMI(temp[t])*I/(S+E+I+R)+mu_t(temp[t]))*M1
     dM2 <- (a(temp[t])*pMI(temp[t])*I/(S+E+I+R))*M1-(PDR(temp[t])+mu_t(temp[t]))*M2
     dM3 <- PDR(temp[t])*M2-mu_t(temp[t])*M3
-    dS <- -a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*.5*S + 23.9*(S/1000)/360 - 5.8*(S/1000)/360
-    dE <- a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*S-(1.0/5.9)*E - 5.8*(E/1000)/360
-    dI <- (1.0/5.9)*E-(1.0/5.0)*I - 5.8*(I/1000)/360
-    dR <- (1.0/5.0)*I - 5.8*(R/1000)/360
+    dS <- -a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*.5*S + 23.9*(S/1000)/360 - 5.8*(S/1000)/360 + ie*(S+E+I+R) - ie*S
+    dE <- a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*S-(1.0/5.9)*E - 5.8*(E/1000)/360  - ie*E
+    dI <- (1.0/5.9)*E-(1.0/5.0)*I - 5.8*(I/1000)/360  - ie*I
+    dR <- (1.0/5.0)*I - 5.8*(R/1000)/360  - ie*R
     list(c(dM1, dM2, dM3, dS, dE, dI, dR))
   })
 }    
@@ -25,10 +25,10 @@ seiseir_model_th <- function(t, state, parameters) {
     dM1 <- (EFD(temp[t])*pEA(temp[t])*MDR(temp[t])*mu_th(temp[t], hum[t])^(-1))*(M1+M2+M3)*max((1-((M1+M2+M3)/K_th(temp[t], hum[t]))),0)-(a(temp[t])*pMI(temp[t])*I/(S+E+I+R)+mu_th(temp[t], hum[t])*M1)
     dM2 <- (a(temp[t])*pMI(temp[t])*I/(S+E+I+R))*M1-(PDR(temp[t])+mu_th(temp[t], hum[t]))*M2
     dM3 <- PDR(temp[t])*M2-mu_th(temp[t], hum[t])*M3
-    dS <- -a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*S + 23.9*(S/1000)/360 - 5.8*(S/1000)/360
-    dE <- a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*S-(1.0/5.9)*E - 5.8*(E/1000)/360
-    dI <- (1.0/5.9)*E-(1.0/5.0)*I - 5.8*(I/1000)/360
-    dR <- (1.0/5.0)*I - 5.8*(R/1000)/360
+    dS <- -a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*S + 23.9*(S/1000)/360 - 5.8*(S/1000)/360 + ie*(S+E+I+R) - ie*S
+    dE <- a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*S-(1.0/5.9)*E - 5.8*(E/1000)/360 - ie*E
+    dI <- (1.0/5.9)*E-(1.0/5.0)*I - 5.8*(I/1000)/360 - ie*I
+    dR <- (1.0/5.0)*I - 5.8*(R/1000)/360 - ie*R
     list(c(dM1, dM2, dM3, dS, dE, dI, dR))
   })
 }    
@@ -44,10 +44,10 @@ seiseir_model_thr <- function(t, state, parameters) {
     dM1 <- (EFD(temp[t])*pEA(temp[t])*MDR(temp[t])*mu_th(temp[t], hum[t])^(-1))*(M1+M2+M3)*max((1-((M1+M2+M3)/K_thr(temp[t], hum[t], rain[t]))),0)-(a(temp[t])*pMI(temp[t])*I/(S+E+I+R)+mu_th(temp[t], hum[t])*M1)
     dM2 <- (a(temp[t])*pMI(temp[t])*I/(S+E+I+R))*M1-(PDR(temp[t])+mu_th(temp[t], hum[t]))*M2
     dM3 <- PDR(temp[t])*M2-mu_th(temp[t], hum[t])*M3
-    dS <- -a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*S + 23.9*(S/1000)/360 - 5.8*(S/1000)/360 + ((0.8*R)/900) #0.16*R.lag
-    dE <- a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*S-(1.0/5.9)*E - 5.8*(E/1000)/360 
-    dI <- (1.0/5.9)*E-(1.0/5.0)*I - 5.8*(I/1000)/360 
-    dR <- (1.0/5.0)*I - 5.8*(R/1000)/360 - ((0.8*R)/900) # 0.16*R.lag
+    dS <- -a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*S + 23.9*(S/1000)/360 - 5.8*(S/1000)/360 + ie*(S+E+I+R) - ie*S#0.16*R.lag
+    dE <- a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*S-(1.0/5.9)*E - 5.8*(E/1000)/360  - ie*E
+    dI <- (1.0/5.9)*E-(1.0/5.0)*I - 5.8*(I/1000)/360 - ie*I
+    dR <- (1.0/5.0)*I - 5.8*(R/1000)/360 - ie*R # 0.16*R.lag
     list(c(dM1, dM2, dM3, dS, dE, dI, dR)) #, "R.lag"=R.lag
   })
 }    
