18b8d9ecfa58ad3e610a925adff6735344a355d7
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2019-08-26 16:14:07 +1000

    Update initialization.

--- Codes/SEI-SEIR_simulate_with_temperature_model.R
+++ Codes/SEI-SEIR_simulate_with_temperature_model.R
@@ -7,35 +7,25 @@ library(deSolve)
 # load model
 source("Codes/SEI-SEIR_model_T.R")
 
-# load climate data
-climateData <- read.csv("Concatenated_Data/climate_data/merged_climate_data.csv", head=T, stringsAsFactors = F)
-climateData$Date <- as.Date(climateData$Date, "%Y-%m-%d")
-
-# load and set initial conditions
-init.cond <- read.csv("Concatenated_Data/sensitivity_analyses/LHS_inputs.csv", head=T)
-startIC <- subset(init.cond, IC == "18")
-
-# set immigration and emmigration rate
-ie <- 0.01
+# load data 
+source("Codes/SEI-SEIR_simulation_setup.R")
 
 # run simulations
-population <- c(7304, 547557, 240698, 154048, 57370, 279890, 13670, 25620)
-sites <- c("Chulaimbo", "Kisumu", "Msambweni", "Ukunda", "Huaquillas", "Machala", "Portovelo", "Zaruma")
-timestep = 1/12
 traitDF <- data.frame(matrix(ncol = 10, nrow = 0))
 colnames(traitDF) <- c("time", "M1", "M2", "M3", "S", "E", "I", "R", "Date", "Site")
 traitFileName <- "Concatenated_Data/model_simulations/SEI-SEIR_simulations_temperature.csv"
 write.csv(traitDF, traitFileName, row.names = F)
 
 for (l in 1:length(sites)){
-  siteclimatevars <- names(climateData)[grep(sites[l], names(climateData))]
-  climateData2 <- climateData[,c("Date", siteclimatevars)]
+  climateData2 <- subset(climateData, Site == sites[l])
   climateData2 <- climateData2[complete.cases(climateData2),]
-  temp <- climateData2[, grep('temp', names(climateData2))]
-  M0 <- K_t(temp[1])
-  Date <- climateData2$Date
+  temp <- climateData2$Temperature
   H0 <- population[l]
+  M0 <- K_t(temp[1], H0)
+  Date <- climateData2$Date
   city <- sites[l]
+  BR <- BRs[l]
+  DR <- DRs[l]
   times <- seq(1,length(Date), by=1)
   parameters <- c(EFD, pEA, MDR, K_t, a, pMI, mu_t, PDR, b, timestep=timestep)
   state <- c(M1 = startIC$m1*M0, M2 = startIC$m2*M0, M3 = startIC$m3*M0, S = startIC$s*H0, E = startIC$e*H0, I = startIC$i*H0, R = startIC$r*H0)
@@ -47,4 +37,3 @@ for (l in 1:length(sites)){
   write.csv(traitDF, traitFileName, row.names = F)
   cat("finished running ode for", sites[l], "\n")
 }
-
