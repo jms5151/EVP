921f390c48303ac4528eca2383c982a5353ad938
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2019-08-26 16:16:11 +1000

    Update rainfall metric and gapfilling.

--- Codes/concat_and_gapfill_climate_data_from_Ecuador.R
+++ Codes/concat_and_gapfill_climate_data_from_Ecuador.R
@@ -19,10 +19,10 @@
 # 2000-01-01 - 2012-12-31 Temperature, rainfall (z0012)
 # 2013-03-20 - 2014-05-16 Temperature, humidity, rainfall (p14r)
 # 2014-05-18 - 2015-05-17 Temperature, humidity, rainfall (p13r)
-# 2016-03-10 - 2017-12-08 Temperature, humidity, rainfall (porotvelo)
+# 2016-03-10 - 2017-12-08 Temperature, humidity, rainfall (portovelo)
 # 2016-06-21 - 2018-07-10 Temperature, humidity, rainfall (p00r)
 # 2018-04-23 - 2019-01-09 Temperature, humidity, rainfall (port1819)
-# 
+#
 # Zaruma:
 # 2000-01-01 - 2012-12-31 Temperature, rainfall (z0012)
 # 2013-03-20 - 2014-05-16 Temperature, humidity, rainfall (z14r)
@@ -71,42 +71,27 @@ for (j in 1:length(ecuador.rdata.climate)){
   load(fileName)
 }
 
-# load and format NOAA GSOD (Global Summary of the Day) from 
-# https://www7.ncdc.noaa.gov/CDO/cdoselect.cmd?datasetabbv=GSOD&countryabbv&georegionabbv
-# ecuador.txt.climate <- ecuador.climate.files[grepl(".txt", ecuador.climate.files)]
-# txtName <- c("GSOD_Atahualpa", "GSOD_Loja", "GSOD_Santa_Rosa_Aeropuerto")
-# 
-# for (k in 1:length(ecuador.txt.climate)){
-#   fileName <- paste0("Ecuador/EVP_Ecuador_Data/climate_data/", ecuador.txt.climate[k])
-#   x <- read.delim(fileName, head=TRUE, sep=',')
-#   x$mean_temp_gsod <- round((x$TEMP-32)*(5/9), 2)
-#   x$Date = as.Date(paste(substr(x$YEARMODA, 1, 4), substr(x$YEARMODA, 5, 6), substr(x$YEARMODA, 7, 8), sep="-"), "%Y-%m-%d")
-#   x$rain_gsod <- as.numeric(gsub("[A-Z]|99.99", "", x$PRCP)) 
-#   x$rain_gsod <- round((x$rain_gsod * 25.4), 2)
-#   x <- x[,c("Date", "mean_temp_gsod", "rain_gsod")]
-#   assign(txtName[k],x)
-# }
+# Repace/remove flagged data
+h00r$RH[(which(h00r$date == "2017-01-31")):(which(h00r$date == "2017-04-14"))] <- NA
+mach$RH[1:(which(mach$date == "2017-12-10"))] <- NA
+port1819 <- port1819[1:(which(port1819$date == '2018-10-10')),]
 
-# format temperature and site
+# merge all datasets by site
 Huaquillas <- do.call(rbind, setNames(lapply(ls(pattern="^h"), function(x) get(x)), ls(pattern="^h")))
 Machala <- do.call(rbind, setNames(lapply(ls(pattern="^m"), function(x) get(x)), ls(pattern="^m")))
 Portovelo <- do.call(rbind, setNames(lapply(ls(pattern="^p"), function(x) get(x)), ls(pattern="^p")))
 Zaruma <- do.call(rbind, setNames(lapply(ls(pattern="^z"), function(x) get(x)), ls(pattern="^z")))
 
-# add column with site name
+# add column to each dataset with site name
 Huaquillas$Site <- "Huaquillas"
 Machala$Site <- "Machala"
 Portovelo$Site <- "Portovelo"
 Zaruma$Site <- "Zaruma"
 
-# replace suspect temperature & humidity values with NA (data issue for Portovelo, ~90 dates with temp of 0 degrees and 1 with 16 degrees)
-Portovelo$temp <- ifelse(Portovelo$temp < 17, NA, Portovelo$temp)
-Zaruma$temp <- ifelse(Zaruma$temp > 30, NA, Zaruma$temp)
-
-Huaquillas$RH <- ifelse(Huaquillas$RH < 48, NA, Huaquillas$RH)
-Machala$RH <- ifelse(Machala$RH < 48, NA, Machala$RH)
-Portovelo$RH <- ifelse(Portovelo$RH < 48, NA, Portovelo$RH)
-Zaruma$RH <- ifelse(Zaruma$RH < 48, NA, Zaruma$RH)
+# replace flagged values with NA
+Portovelo$temp[Portovelo$temp < 20] <- NA
+Zaruma$temp[Zaruma$temp < 20] <- NA
+Zaruma$temp[Zaruma$temp > 32] <- NA
 
 # combine all data
 weather <- do.call(rbind, list(Huaquillas, Machala, Portovelo, Zaruma))
@@ -131,6 +116,14 @@ for (l in 1:length(sites)){
   wide_data <- merge(wide_data, x, by="date", all.x=T)
 }
 
+# for each day, calculate aggregated rainfall values for month prior
+for (j in 1:length(sites)){
+  wide_data[paste0(sites[j], "_Two_week_rainfall")] <- NA
+  for (k in 14:nrow(wide_data)){
+    wide_data[k,paste0(sites[j], "_Two_week_rainfall")] <- sum(wide_data[(k-13):k, paste0(sites[j], "_rain")])
+  }  
+} 
+
 # calculate average value by day and site across all years
 wide_data$Month_Day <- format(wide_data$date, "%m-%d")
 ltm <- wide_data
@@ -139,16 +132,20 @@ ltm <- ddply(ltm, .(Month_Day), summarize
              , Machala_temp_ltm = ifelse(all(is.na(Machala_temp)), NA, mean(Machala_temp, na.rm=T))
              , Portovelo_temp_ltm = ifelse(all(is.na(Portovelo_temp)), NA, mean(Portovelo_temp, na.rm=T))
              , Zaruma_temp_ltm = ifelse(all(is.na(Zaruma_temp)), NA, mean(Zaruma_temp, na.rm=T))
-             , Huaquillas_rain_ltm = ifelse(all(is.na(Huaquillas_rain)), NA, mean(Huaquillas_rain, na.rm=T))
-             , Machala_rain_ltm = ifelse(all(is.na(Machala_rain)), NA, mean(Machala_rain, na.rm=T))
-             , Portovelo_rain_ltm = ifelse(all(is.na(Portovelo_rain)), NA, mean(Portovelo_rain, na.rm=T))
-             , Zaruma_rain_ltm = ifelse(all(is.na(Zaruma_rain)), NA, mean(Zaruma_rain, na.rm=T))
+             , Huaquillas_rain_ltm = ifelse(all(is.na(Huaquillas_Two_week_rainfall)), NA, mean(Huaquillas_Two_week_rainfall, na.rm=T))
+             , Machala_rain_ltm = ifelse(all(is.na(Machala_Two_week_rainfall)), NA, mean(Machala_Two_week_rainfall, na.rm=T))
+             , Portovelo_rain_ltm = ifelse(all(is.na(Portovelo_Two_week_rainfall)), NA, mean(Portovelo_Two_week_rainfall, na.rm=T))
+             , Zaruma_rain_ltm = ifelse(all(is.na(Zaruma_Two_week_rainfall)), NA, mean(Zaruma_Two_week_rainfall, na.rm=T))
              , Huaquillas_rh_ltm = ifelse(all(is.na(Huaquillas_humidity)), NA, mean(Huaquillas_humidity, na.rm=T))
              , Machala_rh_ltm = ifelse(all(is.na(Machala_humidity)), NA, mean(Machala_humidity, na.rm=T))
              , Portovelo_rh_ltm = ifelse(all(is.na(Portovelo_humidity)), NA, mean(Portovelo_humidity, na.rm=T))
              , Zaruma_rh_ltm = ifelse(all(is.na(Zaruma_humidity)), NA, mean(Zaruma_humidity, na.rm=T)))
 
 wide_data <- merge(wide_data, ltm, by="Month_Day")
+
+# save pre-gapfilled weather data
+# x<-wide_data[,c("date", "Machala_temp", "Huaquillas_temp", "Portovelo_temp", "Zaruma_temp", "Machala_Two_week_rainfall", "Huaquillas_Two_week_rainfall", "Portovelo_Two_week_rainfall", "Zaruma_Two_week_rainfall", "Machala_humidity", "Huaquillas_humidity", "Portovelo_humidity", "Zaruma_humidity")]
+# write.csv(x, "Concatenated_Data/climate_data/pregapfilled_data_Ecuador.csv", row.names = F)
 
 # fill in missing temperature data -----------------------------------------------------------------
 # Huaquillas
@@ -169,49 +166,70 @@ wide_data$Portovelo_Temperature <- ifelse(!is.na(wide_data$Portovelo_Temperature
 # Zaruma
 fill_za_w_po <- lm(Zaruma_temp ~ Portovelo_temp, data = wide_data)
 wide_data$Zaruma_Temperature <- ifelse(!is.na(wide_data$Zaruma_temp), wide_data$Zaruma_temp, round(coef(fill_za_w_po)[[1]] + coef(fill_za_w_po)[[2]] * wide_data$Portovelo_temp, 1))
-wide_data$Zaruma_Temperature <- ifelse(!is.na(wide_data$Zaruma_Temperature), wide_data$Zaruma_Temperature, wide_data$Portovelo_temp_ltm)
+wide_data$Zaruma_Temperature <- ifelse(!is.na(wide_data$Zaruma_Temperature), wide_data$Zaruma_Temperature, wide_data$Zaruma_temp_ltm)
 
 # fill in missing rainfall data -----------------------------------------------------------------
-wide_data$Huaquillas_Rainfall <- ifelse(!is.na(wide_data$Huaquillas_rain), wide_data$Huaquillas_rain, wide_data$Huaquillas_rain_ltm)
-wide_data$Machala_Rainfall <- ifelse(!is.na(wide_data$Machala_rain), wide_data$Machala_rain, wide_data$Machala_rain_ltm)
-wide_data$Portovelo_Rainfall <- ifelse(!is.na(wide_data$Portovelo_rain), wide_data$Portovelo_rain, wide_data$Portovelo_rain_ltm)
-# fill in feb 29th's with feb 28
-wide_data$Portovelo_Rainfall <- ifelse(!is.na(wide_data$Portovelo_Rainfall), wide_data$Portovelo_Rainfall, mean(wide_data$Portovelo_Rainfall[wide_data$Month_Day=='02-28']))
-wide_data$Zaruma_Rainfall <- ifelse(!is.na(wide_data$Zaruma_rain), wide_data$Zaruma_rain, wide_data$Zaruma_rain_ltm)
+# Huaquillas
+fill_ha_w_ma <- lm(Huaquillas_Two_week_rainfall ~ Machala_Two_week_rainfall, data = wide_data)
+wide_data$Huaquillas_Two_week_rainfall <- ifelse(!is.na(wide_data$Huaquillas_Two_week_rainfall), wide_data$Huaquillas_Two_week_rainfall, round(coef(fill_ha_w_ma)[[1]] + coef(fill_ha_w_ma)[[2]] * wide_data$Machala_Two_week_rainfall, 1))
+wide_data$Huaquillas_Two_week_rainfall <- ifelse(!is.na(wide_data$Huaquillas_Two_week_rainfall), wide_data$Huaquillas_Two_week_rainfall, wide_data$Huaquillas_rain_ltm)
+
+# Machala
+fill_ma_w_ha <- lm(Machala_Two_week_rainfall ~ Huaquillas_Two_week_rainfall, data = wide_data)
+wide_data$Machala_Two_week_rainfall <- ifelse(!is.na(wide_data$Machala_Two_week_rainfall), wide_data$Machala_Two_week_rainfall, round(coef(fill_ma_w_ha)[[1]] + coef(fill_ma_w_ha)[[2]] * wide_data$Huaquillas_Two_week_rainfall, 1))
+# wide_data$Machala_Two_week_rainfall <- ifelse(!is.na(wide_data$Machala_Two_week_rainfall), wide_data$Machala_Two_week_rainfall, wide_data$Machala_rain_ltm)
+
+# Portovelo
+fill_po_w_za <- lm(Portovelo_Two_week_rainfall ~ Zaruma_Two_week_rainfall, data = wide_data)
+wide_data$Portovelo_Two_week_rainfall <- ifelse(!is.na(wide_data$Portovelo_Two_week_rainfall), wide_data$Portovelo_Two_week_rainfall, round(coef(fill_po_w_za)[[1]] + coef(fill_po_w_za)[[2]] * wide_data$Zaruma_Two_week_rainfall, 1))
+wide_data$Portovelo_Two_week_rainfall <- ifelse(!is.na(wide_data$Portovelo_Two_week_rainfall), wide_data$Portovelo_Two_week_rainfall, wide_data$Portovelo_rain_ltm)
+
+# Zaruma
+fill_za_w_po <- lm(Zaruma_Two_week_rainfall ~ Portovelo_Two_week_rainfall, data = wide_data)
+wide_data$Zaruma_Two_week_rainfall <- ifelse(!is.na(wide_data$Zaruma_Two_week_rainfall), wide_data$Zaruma_Two_week_rainfall, round(coef(fill_za_w_po)[[1]] + coef(fill_za_w_po)[[2]] * wide_data$Portovelo_Two_week_rainfall, 1))
+# wide_data$Zaruma_Two_week_rainfall <- ifelse(!is.na(wide_data$Zaruma_Two_week_rainfall), wide_data$Zaruma_Two_week_rainfall, wide_data$Zaruma_rain_ltm)
+# replace values below zero with zero (due to negative coefficient value in regression)
+wide_data$Zaruma_Two_week_rainfall <- ifelse(wide_data$Zaruma_Two_week_rainfall < 0, 0, wide_data$Zaruma_Two_week_rainfall)
 
 # fill in missing humidity data -----------------------------------------------------------------
-wide_data$Huaquillas_Humidity <- ifelse(!is.na(wide_data$Huaquillas_humidity), wide_data$Huaquillas_humidity, wide_data$Huaquillas_rh_ltm)
-wide_data$Machala_Humidity <- ifelse(!is.na(wide_data$Machala_humidity), wide_data$Machala_humidity, wide_data$Machala_rh_ltm)
-wide_data$Portovelo_Humidity <- ifelse(!is.na(wide_data$Portovelo_humidity), wide_data$Portovelo_humidity, wide_data$Portovelo_rh_ltm) # still four days missing
+# Huaquillas
+fill_ha_w_ma <- lm(Huaquillas_humidity ~ Machala_humidity, data = wide_data)
+wide_data$Huaquillas_Humidity <- ifelse(!is.na(wide_data$Huaquillas_humidity), wide_data$Huaquillas_humidity, round(coef(fill_ha_w_ma)[[1]] + coef(fill_ha_w_ma)[[2]] * wide_data$Machala_humidity, 1))
+wide_data$Huaquillas_Humidity <- ifelse(!is.na(wide_data$Huaquillas_Humidity), wide_data$Huaquillas_Humidity, wide_data$Huaquillas_rh_ltm)
+
+# Machala
+fill_ma_w_ha <- lm(Machala_humidity ~ Huaquillas_humidity, data = wide_data)
+wide_data$Machala_Humidity <- ifelse(!is.na(wide_data$Machala_humidity), wide_data$Machala_humidity, round(coef(fill_ma_w_ha)[[1]] + coef(fill_ma_w_ha)[[2]] * wide_data$Huaquillas_humidity, 1))
+wide_data$Machala_Humidity <- ifelse(!is.na(wide_data$Machala_Humidity), wide_data$Machala_Humidity, wide_data$Machala_rh_ltm)
+
+# Portovelo
+fill_po_w_za <- lm(Portovelo_humidity ~ Zaruma_humidity, data = wide_data)
+wide_data$Portovelo_Humidity <- ifelse(!is.na(wide_data$Portovelo_humidity), wide_data$Portovelo_humidity, round(coef(fill_po_w_za)[[1]] + coef(fill_po_w_za)[[2]] * wide_data$Zaruma_humidity, 1))
+wide_data$Portovelo_Humidity <- ifelse(!is.na(wide_data$Portovelo_Humidity), wide_data$Portovelo_Humidity, wide_data$Portovelo_rh_ltm)
 # fill in feb 29th's with feb 28
 wide_data$Portovelo_Humidity <- ifelse(!is.na(wide_data$Portovelo_Humidity), wide_data$Portovelo_Humidity, mean(wide_data$Portovelo_Humidity[wide_data$Month_Day=='02-28']))
-wide_data$Zaruma_Humidity <- ifelse(!is.na(wide_data$Zaruma_humidity), wide_data$Zaruma_humidity, wide_data$Zaruma_rh_ltm)
 
-# calculate 30 day aggregated rainfall values ---------------------------------------------------
+# Zaruma
+fill_za_w_po <- lm(Zaruma_humidity ~ Portovelo_humidity, data = wide_data)
+wide_data$Zaruma_Humidity <- ifelse(!is.na(wide_data$Zaruma_humidity), wide_data$Zaruma_humidity, round(coef(fill_za_w_po)[[1]] + coef(fill_za_w_po)[[2]] * wide_data$Portovelo_humidity, 1))
+wide_data$Zaruma_Humidity <- ifelse(!is.na(wide_data$Zaruma_Humidity), wide_data$Zaruma_Humidity, wide_data$Zaruma_rh_ltm)
+
+# merge data into long format and save -----------------------------------------------------------
 wide_data <- wide_data[order(wide_data$date),]
 
-for (m in 1:length(sites)){
-  weatherdf <- wide_data[, c("date", paste0(sites[m], "_Temperature"), paste0(sites[m], "_Rainfall"), paste0(sites[m], "_Humidity"))]
-  weatherdf <- weatherdf[order(weatherdf$date),]
-  weatherdf$Monthly_rainfall <- NA
-  weatherdf$Monthly_rainfall_weighted <- NA
-  weatherdf$Monthly_rainy_days_25 <- NA
-  for (n in 30:nrow(weatherdf)){
-    rainSub <- subset(weatherdf, date >= date[n] - 29 & date <= date[n])
-    rainSub$exDec <- 30:1
-    rainSub$exDec <- rainSub[,paste0(sites[m], "_Rainfall")] * (1/rainSub$exDec) 
-    weatherdf$Monthly_rainfall[n] <- sum(rainSub[paste0(sites[m], "_Rainfall")])
-    weatherdf$Monthly_rainfall_weighted[n] <- sum(rainSub$exDec)
-    weatherdf$Monthly_rainy_days_25[n] <- sum(rainSub[paste0(sites[m], "_Rainfall")] > 2.5)
-  }
-  colnames(weatherdf) <- gsub(paste0(sites[m], "_"), "", colnames(weatherdf))
-  weatherdf$Site <- sites[m]
-  weatherdf <- weatherdf[30:nrow(weatherdf),]
-  assign(sites[m], weatherdf)
+for (j in 1:length(sites)){
+  weatherdf <- wide_data[, c("date", paste0(sites[j], "_Temperature"), paste0(sites[j], "_Two_week_rainfall"), paste0(sites[j], "_Humidity"))]
+  colnames(weatherdf) <- gsub(paste0(sites[j], "_"), "", colnames(weatherdf))
+  weatherdf$Site <- paste0(toupper(substr(sites[j],1,1)), substr(sites[j],2,nchar(sites[j])))
+  assign(sites[j], weatherdf)
 }  
 
 # merge data into long format and save -----------------------------------------------------------
 weatherdf <- do.call(rbind, list(Huaquillas, Machala, Portovelo, Zaruma))
 colnames(weatherdf)[1] <- "Date"
-weatherdf[,c(2:7)] <- lapply(weatherdf[,c(2:7)], function(x) round(x,2))
+portDiff <- mean(weatherdf$Temperature[weatherdf$Site=="Portovelo"&weatherdf$Date<"2013-05-06"]) - mean(weatherdf$Temperature[weatherdf$Site=="Portovelo"&weatherdf$Date>"2013-05-06"])# update portovelo weather
+weatherdf$Temperature <- ifelse(weatherdf$Site == "Portovelo" & weatherdf$Date > "2013-05-06", weatherdf$Temperature+portDiff, weatherdf$Temperature)
+weatherdf[,c(2:4)] <- lapply(weatherdf[,c(2:4)], function(x) round(x,2))
 write.csv(weatherdf, "Concatenated_Data/climate_data/gapfilled_climate_data_Ecuador.csv", row.names = F)
+
+
