078c06c074f73eaec22bc5537c1cfbe3a7b1c078
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2019-08-26 16:15:32 +1000

    Update rainfall metric.

--- Codes/assess_models_with_diff_rainfall_functions.R
+++ Codes/assess_models_with_diff_rainfall_functions.R
@@ -10,13 +10,14 @@ cases <- read.csv("Concatenated_Data/case_data/merged_case_data.csv", head=T, st
 vectors <- read.csv("Concatenated_Data/vector_data/merged_vector_data.csv", head=T, stringsAsFactors = F)
 
 # merge observed data
-data <- merge(cases, vectors, by=c("Site", "Date"), all=T)
+data <- merge(cases, vectors, by=c("Site", "Date", "Year.Month"), all=T)
 data$Country <- ifelse(data$Site=="Chulaimbo"|data$Site=="Kisumu"|data$Site=="Msambweni"|data$Site=="Ukunda", "Kenya", "Ecuador")
 
 # load modeled data with different rainfall functions
 models <- read.csv("Concatenated_Data/model_simulations/SEI-SEIR_simulations_THR_diff_rain_functions.csv", head=T, stringsAsFactors = F)
 models$Mtot <- models$M1 + models$M2 + models$M3
 models$Country <- ifelse(models$Site=="Chulaimbo"|models$Site=="Kisumu"|models$Site=="Msambweni"|models$Site=="Ukunda", "Kenya", "Ecuador")
+models$R_function2 <- paste(models$R_function, models$Rain_metric, models$Rmax, sep="_")
 
 # function
 tau.calc <- function(x, y) {
@@ -25,21 +26,20 @@ tau.calc <- function(x, y) {
 }
 
 # set up dataframe
-corr.rainfall <- data.frame(matrix(ncol=12, nrow=0))
-colnames(corr.rainfall) <- c("Region", "Yvar", "tau_cum_exp_de", "tau_cum_exp_in", "tau_cum_quadratic", "tau_cum_rskewed", "tau_cum_briere", "tau_days_exp_de", "tau_days_exp_in", "tau_days_qudratic", "tau_days_rskewed", "tau_days_briere")
+corr.rainfall <- data.frame(matrix(ncol=7, nrow=0))
+colnames(corr.rainfall) <- c("Region", "Yvar", "tau", "Rain_metric", "R_function", "Rmax", "R_Name")
 
 regionnames <- c("Kenya", "Ecuador")
-rainmetrics <- unique(models$Rain_metric)
-rfunctions <- unique(models$R_function)
+rfunctions <- unique(models$R_function2)
 
 for (m in 1:length(regionnames)){
   datasub <- subset(data, Country == regionnames[m])
   modelsub <- subset(models, Country == regionnames[m])
   rainDF <- merge(datasub, modelsub, by=c("Country", "Date", "Site"), all=T)
   if (regionnames[m]=="Ecuador"){
-    yVars <- c("denv_positive_weekly_any", "denv_positive_0311_weekly", "denv_positive_1418_weekly", "denv_positive_weekly", "chikv_positive_weekly", "aedes_total_weekly")
+    yVars <- c("denv_positive", "denv_positive_clinically_diagnosed", "chikv_positive", "aedes_total")
   } else {
-    yVars <- c("denv_positive_weekly", "chikv_positive_weekly", "aedes_total_weekly", "pupae_total_weekly", "late_instar_total_weekly", "early_instar_total_weekly", "egg_total_weekly")
+    yVars <- c("denv_positive", "chikv_positive", "aedes_total", "aedes_total_bg", "pupae_total", "late_instar_total", "early_instar_total", "egg_total")
   }  
   for (n in 1:length(yVars)){
     firstLetter <- substr(yVars[n],1,1)
@@ -48,20 +48,35 @@ for (m in 1:length(regionnames)){
     } else {
       modVar <- "Mtot"
     }
-    tmpvec <- c()
-    for (o in 1:length(rainmetrics)){
-      for (p in 1:length(rfunctions)){
-        tempRainDF <- subset(rainDF, Rain_metric == rainmetrics[o])
-        tempRainDF <- subset(tempRainDF, rfunctions == R_function[p])
-        tempRainDF <- tempRainDF[c(yVars[n], modVar)]
-        tempRainDF <- tempRainDF[complete.cases(tempRainDF),]
-        tau <- tau.calc(tempRainDF[,yVars[n]], tempRainDF[,modVar])
-        tmpvec <- c(tmpvec, round(tau, 2))
+    for (o in 1:length(rfunctions)){
+      tempRainDF <- subset(rainDF, R_function2 == rfunctions[o])
+      Rain_metric <- unique(tempRainDF$Rain_metric)
+      R_function <- unique(tempRainDF$R_function)
+      Rmax <- unique(tempRainDF$Rmax)
+      tempRainDF <- tempRainDF[,c(yVars[n], modVar)]
+      tempRainDF <- tempRainDF[complete.cases(tempRainDF),]
+      if (nrow(tempRainDF) == 0){
+        tau <- NA
+      } else {
+        tau <- round(tau.calc(tempRainDF[,yVars[n]], tempRainDF[,modVar]), 2)
       }
+      tmpvec <- c(regionnames[m], yVars[n], tau, Rain_metric, R_function, Rmax, rfunctions[o])
+      corr.rainfall[nrow(corr.rainfall)+1,] <- tmpvec
     }
-    tmpvec <- c(regionnames[m], yVars[n], tmpvec)
-    corr.rainfall[nrow(corr.rainfall)+1,] <- tmpvec
   }
 }
 
 write.csv(corr.rainfall, "Concatenated_Data/model_assessment/tau_diff_rain_functions.csv", row.names=F)
+corr.rainfall <- read.csv("Concatenated_Data/model_assessment/tau_diff_rain_functions.csv", head=T)
+
+library(tidyverse)
+best <- corr.rainfall %>% 
+  group_by(Region, Yvar)  %>%  
+  filter(tau == max(tau, na.rm=T))
+
+library(ggplot2)
+par(mar=c(5.1,10,4.1,2.1))
+df <- subset(corr.rainfall, Yvar == "denv_positive" & Region == "Ecuador")
+dotchart(as.numeric(best$tau), labels=best$Yvar, cex=1.2, groups= as.factor(best$Region)
+         , xlab="Tau", pt.cex=2, gcolor="black", pch = 16)
+
