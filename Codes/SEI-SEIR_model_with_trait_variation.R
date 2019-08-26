3c3feed884f88853a53b3d47e6865a46084f5879
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2019-08-26 16:12:24 +1000

    Update rainfall and humidity functions.

--- Codes/SEI-SEIR_model_with_trait_variation.R
+++ Codes/SEI-SEIR_model_with_trait_variation.R
@@ -1,7 +1,7 @@
 # SEI-SEIR model with temperature, humidity, and rainfall --------------------------------
 seiseir_model_thr <- function(t, state, parameters) {
   with(as.list(c(state,parameters)), {
-    dM1 <- (EFD(temp[t])*pEA(temp[t])*MDR(temp[t])*mu_th(temp[t], hum[t])^(-1))*(M1+M2+M3)*max((1-((M1+M2+M3)/K_thr(temp[t], hum[t], rain[t]))),0)-(a(temp[t])*pMI(temp[t])*I/(S+E+I+R)+mu_th(temp[t], hum[t])*M1)
+    dM1 <- (EFD(temp[t])*pEA(temp[t])*MDR(temp[t])*mu_th(temp[t], hum[t])^(-1))*(M1+M2+M3)*max((1-((M1+M2+M3)/K_trh(temp[t], hum[t], rain[t], Rmax, (S+E+I+R)))),0)-(a(temp[t])*pMI(temp[t])*I/(S+E+I+R)+mu_th(temp[t], hum[t])*M1)
     dM2 <- (a(temp[t])*pMI(temp[t])*I/(S+E+I+R))*M1-(PDR(temp[t])+mu_th(temp[t], hum[t]))*M2
     dM3 <- PDR(temp[t])*M2-mu_th(temp[t], hum[t])*M3
     dS <- -a(temp[t])*b(temp[t])*(M3/(M1+M2+M3+0.001))*S + BR*(S/1000)/360 - DR*(S/1000)/360 + ie*(S+E+I+R) - ie*S
@@ -38,49 +38,45 @@ inverted_quadratic <- function(x, c, T0, Tm){
 
 # eggs per female per day
 EFD <- function(temp){
-  # briere(temp,8.56e-03,14.58,34.61)
   briere(temp, EFD_c, EFD_T0, EFD_Tm)
 }
 
 # probability egg to adult survival
 pEA <- function(temp){
-  # quadratic(temp,-5.99e-03,13.56,38.29)
   quadratic(temp, pEA_c, pEA_T0, pEA_Tm)
 }
 
 # mosquito development rate (1/larval development period)
 MDR <- function(temp){
-  # briere(temp,7.86e-05,11.36,39.17)
   briere(temp, MDR_c, MDR_T0, MDR_Tm)
 }
 
 # biting rate
 a <- function(temp){
-  # briere(temp,2.02e-04,13.35,40.08)
   briere(temp, a_c, a_T0, a_Tm)
 }
 
 # probability	of mosquito	infection per	bite	on	an	infectious	host
 pMI <- function(temp){
-  # briere(temp,4.91e-04,12.22,37.46)
   briere(temp, pMI_c, pMI_T0, pMI_Tm)
 }
 
 # adult mosquito mortality rate (1/adult lifespan)
 mu_th <- function(temp, hum){
-  # inverted_quadratic(temp,-1.48e-01,9.16,37.73+0.643*(hum/100))
-  inverted_quadratic(temp, mu_th_c, mu_th_T0, mu_th_Tm + 0.643*(hum/100))
+  if (hum <= 1){
+    inverted_quadratic(temp, mu_th_c, mu_th_T0, mu_th_Tm)+(1-(0.01256 + 2.00893*hum))*0.01
+  } else {
+    inverted_quadratic(temp, mu_th_c, mu_th_T0, mu_th_Tm)+(1-(1.2248 + 0.2673*hum))*0.01
+  }
 }
 
 # parasite development rate
 PDR <- function(temp){
-  # briere(temp,6.56e-05,10.68,45.90)
   briere(temp, PDR_c, PDR_T0, PDR_Tm)
 }
 
 # transmission competence: probability of human	infection	per	bite	by	an	infectious mosquito
 b <- function(temp){
-  # briere(temp,8.49e-04,17.05,35.83)
   briere(temp, b_c, b_T0, b_Tm)
 }
 
@@ -91,13 +87,39 @@ carrying_capacity_th <- function(temp, T0, EA, N, h0){
   (alpha*N*exp(-EA*((temp-T0)^2)/(kappa*(temp+273.0)*(T0+273.0))))
 }
 
-K_thr <- function(temp, hum, rain){
-  if(rain >= 55){
-    0.1*carrying_capacity_th(temp,29.0,0.05,20000, hum)
+# K_trh <- function(temp, hum, rain, N){
+#   max(carrying_capacity_th(temp, 29.0, 0.05, N, hum)*(1.126 + 9.094e-03*rain + -5.933e-04*rain^2 + 5.951e-06*rain^3 + -1.892e-08*rain^4), 1000)
+# }
+
+K_trh_right_skewed <- function(temp, h0, rain, Rmax, N){
+  R0 <- 1
+  if((rain < R0) | (rain > Rmax)){
+    max(0.01*carrying_capacity_th(temp, 29.0, 0.05, N, h0), 1000)
   }
   else {
-    # (1.267876e-05*rain*(rain-0)*sqrt(150-rain))*K_th(temp, hum) # briere
-    carrying_capacity_th(temp,29.0,0.05,20000, hum)*(rain/55) # linear
-    # K_th(temp, hum)*exp(rain) # exponential
+    c <- 7.86e-10
+    max(carrying_capacity_th(temp,29.0,0.05, N, h0)*c*rain*(rain-R0)*exp((Rmax-rain)/15)/100000 + 0.001, 1000)
+  }
+}
+
+K_tr_quadratic <- function(temp, rain, Rmax, N){
+  R0 <- 1
+  if((rain < R0) | (rain > Rmax)){
+    max(0.01*carrying_capacity_t(temp, 29.0, 0.05, N), 1000)
+  }
+  else {
+    c <- -5.99e-03
+    max(carrying_capacity_t(temp, 29.0, 0.05, N)*(c*(rain-R0)*(rain-Rmax))/2 + 0.001, 1000)
+  }
+}
+
+K_trh_briere <- function(temp, h0, rain, Rmax, N){
+  R0 <- 1
+  if((rain < R0) | (rain > Rmax)){
+    max(0.01*carrying_capacity_th(temp, 29.0, 0.05, N, h0), 1000)
+  }
+  else {
+    c <- 7.86e-05
+    max(carrying_capacity_th(temp,29.0,0.05, N, h0)*c*rain*(rain-R0)*sqrt(Rmax-rain)*0.3 + 0.001, 1000)
   }
 }
