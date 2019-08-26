af21aa37b7268c5a418f86953f68744113a4b08f
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2019-08-26 16:13:52 +1000

    Re-write loop.

--- Codes/SEI-SEIR_simulate_with_initial_condition_variation.R
+++ Codes/SEI-SEIR_simulate_with_initial_condition_variation.R
@@ -6,59 +6,52 @@ library(dplyr)
 library(deSolve)
 
 # load model 
-source("Codes/SEI-SEIR_model_THR.R")
+source("Codes/SEI-SEIR_model_TRH.R")
 
 # load data 
-climateData <- read.csv("Concatenated_Data/climate_data/merged_climate_data.csv", head=T)
-climateData$Date <- as.Date(climateData$Date, "%Y-%m-%d")
-
-initCond <- read.csv("Concatenated_Data/sensitivity_analyses/LHS_inputs.csv", head=T, stringsAsFactors = F)
+source("Codes/SEI-SEIR_simulation_setup.R")
 
 # run simulations using different initial conditions for state variables and save results -------------
-population <- c(7304, 547557, 240698, 154048, 57370, 279890, 13670, 25620)
-sites <- c("Chulaimbo", "Kisumu", "Msambweni", "Ukunda", "Huaquillas", "Machala", "Portovelo", "Zaruma")
-BRs <- c(rep(31.31,4),rep(20.18,4)) # birth rates from https://data.worldbank.org/indicator/SP.DYN.CBRT.IN
-DRs <- c(rep(5.73,4),rep(5.12,4)) # death rates from https://data.worldbank.org/indicator/SP.DYN.CBRT.IN
-ie <- 0.01 # immigration/emigration rate
-timestep = 1/12
 initDF <- data.frame(matrix(ncol = 11, nrow = 0))
-colnames(initDF) <- c("time", "M1", "M2", "M3", "S", "E", "I", "R", "Date", "Site", "InitCond")  
+colnames(initDF) <- c("time", "M1", "M2", "M3", "S", "E", "I", "R", "Date", "Site", "InitCond")
 fileName <- "Concatenated_Data/sensitivity_analyses/Initital_condition_simulations.csv"
 write.csv(initDF, fileName, row.names=F)
 
 # run model simulations
-for (j in 1:length(sites)){
-  for (k in 1:nrow(initCond)){
-    siteclimatevars <- names(climateData)[grep(sites[i], names(climateData))]
-    climateData2 <- climateData[,c("Date", siteclimatevars)]
-    climateData2 <- climateData2[complete.cases(climateData2),]
-    temp <- climateData2[, grep('temp', names(climateData2))]
-    hum <- climateData2[, grep('humidity', names(climateData2))]
-    rain <- climateData2[, grep('cumRain', names(climateData2))]
-    M0 <- K_thr(temp[1], hum[1], rain[1]) # this changes depending on model!
-    Date <- climateData2$Date
-    H0 <- population[j]
-    city <- sites[j]
-    times <- seq(1,length(Date), by=1)
-    parameters <- c(EFD, pEA, MDR, K_thr, a, pMI, mu_th, PDR, b, timestep=timestep) # this changes too!
-    state <- c(M1 = initCond$m1[k]*M0, M2 = initCond$m2[k]*M0, M3 = initCond$m3[k]*M0, S = initCond$s[k]*H0, E = initCond$e[k]*H0, I = initCond$i[k]*H0, R = initCond$r[k]*H0)
-    out <- ode(y = state, times = times, func = seiseir_model_r, parms = parameters, method="rk4", atol = 1e-14, rtol = 1e-14, hini = timestep)
+for (l in 1:length(sites)){
+  climateData2 <- subset(climateData, Site == sites[l])
+  climateData2 <- climateData2[order(climateData2$Date),]
+  climateData2 <- climateData2[complete.cases(climateData2),]
+  temp <- climateData2$Temperature
+  rain <- climateData2$Two_week_rainfall
+  Rmax <- 123
+  if (unique(climateData2$country) == "Ecuador"){
+    K_trh <- K_trh_quadratic
+  } else {
+    K_trh <- K_trh_briere
+  }
+  hum <- climateData2$SVPD
+  Date <- climateData2$Date
+  H0 <- population[l]
+  city <- sites[l]
+  BR <- BRs[l]
+  DR <- DRs[l]
+  times <- seq(1,length(Date), by=1)
+  M0 <- K_trh(temp[1], hum[1], mean(rain), Rmax, H0)
+  parameters <- c(EFD, pEA, MDR, K_trh, a, pMI, mu_th, PDR, b, timestep=timestep)
+  for (k in 1:nrow(init.cond)){
+    state <- c(M1 = init.cond$m1[k]*M0, M2 = init.cond$m2[k]*M0, M3 = init.cond$m3[k]*M0, S = init.cond$s[k]*H0, E = init.cond$e[k]*H0, I = init.cond$i[k]*H0, R = init.cond$r[k]*H0)
+    out <- ode(y = state, times = times, func = seiseir_model_thr, parms = parameters, method="rk4", atol = 1e-14, rtol = 1e-14, hini = timestep)
     out2 <- as.data.frame(out)
     out2$Date <- Date
-    out2$Site <- sites[j]
-    out2$IC <- c(initCond$IC[k])
-    initDF <- rbind(initDF, out2)
-    write.csv(initDF, fileName, row.names = F)
-    cat("finished running ode for", sites[j], "Initial conditions =", initCond$IC[k], "\n")
+    out2$Site <- sites[l]
+    out2$IC <- c(init.cond$IC[k])
+    write.table(out2, file=fileName, row.names = F, sep = ",", col.names = !file.exists(fileName), append = T)
+    cat("finished running ode for", sites[l], "Initial conditions =", init.cond$IC[k], "\n")
   }
 }
 
-# select initial conditions for future simulations -------------------------------------------
-inits <- read.csv("Concatenated_Data/sensitivity_analyses/LHS_inputs.csv", head=T)
-startIC <- subset(inits, IC == "18")
-
 # run simulations using different start dates and save results -------------------------------
-# this will differ between kenya and ecuador, not sure if it is worth doing with ecuador
 startDate <- seq.Date(min(climateData$Date),(min(climateData$Date)+168),by="weeks")
 initSDDF <- data.frame(matrix(ncol = 11, nrow = 0))
 colnames(initSDDF) <- c("time", "M1", "M2", "M3", "S", "E", "I", "R", "Date", "Site", "StartDate")  
