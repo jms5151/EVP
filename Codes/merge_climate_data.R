f708cef8e3392d1cc0ae44e3a0c1da435371fecb
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2019-08-26 16:19:06 +1000

    Add country name and calculation of SVPD.

--- Codes/merge_climate_data.R
+++ Codes/merge_climate_data.R
@@ -4,15 +4,27 @@ rm(list=ls()) #remove previous variable assignments
 # load data
 # source("Codes/concat_and_gapfill_climate_data_from_Kenya.R")
 # source("Codes/concat_and_gapfill_climate_data_from_Ecuador.R")
-climateDataEcuador <- read.csv("Concatenated_Data/climate_data/gapfilled_climate_data_Ecuador_2000-2018.csv", head=T)
-climateDataKenya <- read.csv("Concatenated_Data/climate_data/gapfilled_climate_data_Kenya.csv", head=T)
+climateDataEcuador <- read.csv("Concatenated_Data/climate_data/gapfilled_climate_data_Ecuador.csv", head=T, stringsAsFactors = F)
+climateDataKenya <- read.csv("Concatenated_Data/climate_data/gapfilled_climate_data_Kenya.csv", head=T, stringsAsFactors = F)
 
 # merge data
-climateData <- merge(climateDataKenya, climateDataEcuador, by="Date", all=T)
+climateData <- rbind(climateDataKenya, climateDataEcuador)
 
 # sort data
 climateData$Date <- as.Date(climateData$Date, "%Y-%m-%d")
-climateData <- climateData[order(climateData$Date),]
+climateData <- climateData[order(climateData$Date, climateData$Site),]
+
+# add county 
+climateData$country <- ifelse(climateData$Site=="Chulaimbo"|climateData$Site=="Kisumu"|climateData$Site=="Msambweni"|climateData$Site=="Ukunda", "Kenya", "Ecuador")
+
+# adjust RH if above 100
+climateData$Humidity <- ifelse(climateData$Humidity > 100, 100, climateData$Humidity)
+
+# calculate saturation vapor pressure deficit
+# http://cronklab.wikidot.com/calculation-of-vapour-pressure-deficit
+climateData$SVP <- 610.7*107.5*climateData$Temperature/(237.3+climateData$Temperature)
+climateData$SVPD = (((100 - climateData$Humidity)/100)*climateData$SVP)/1000
+climateData$SVP <- NULL
 
 # save data
-write.csv(climateData, "Concatenated_Data/climate_data/merged_climate_data.csv", row.names = F)
+save(climateData, file="Concatenated_Data/climate_data/merged_climate_data.RData")
