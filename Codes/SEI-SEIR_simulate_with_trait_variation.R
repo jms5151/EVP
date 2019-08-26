57ab8961f6c55a64b223e91be04533544662c624
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2019-08-26 16:14:17 +1000

    Update initialization.

--- Codes/SEI-SEIR_simulate_with_trait_variation.R
+++ Codes/SEI-SEIR_simulate_with_trait_variation.R
@@ -7,29 +7,14 @@ library(deSolve)
 # load model
 source("Codes/SEI-SEIR_model_with_trait_variation.R")
 
-# load climate data
-climateData <- read.csv("Concatenated_Data/climate_data/merged_climate_data.csv", head=T, stringsAsFactors = F)
-climateData$Date <- as.Date(climateData$Date, "%Y-%m-%d")
-
-# load and set initial conditions
-init.cond <- read.csv("Concatenated_Data/sensitivity_analyses/LHS_inputs.csv", head=T)
-startIC <- subset(init.cond, IC == "18")
-
-# load traits
-trait_posterior <- read.csv("Concatenated_Data/sensitivity_analyses/Random_sample_of_posterior_traits.csv", head=T)
-
-# set immigration and emmigration rate
-ie <- 0.01
+# load data 
+source("Codes/SEI-SEIR_simulation_setup.R")
 
 # run simulations
-population <- c(7304, 547557, 240698, 154048, 57370, 279890, 13670, 25620)
-sites <- c("Chulaimbo", "Kisumu", "Msambweni", "Ukunda", "Huaquillas", "Machala", "Portovelo", "Zaruma")
-timestep = 1/12
 traitDF <- data.frame(matrix(ncol = 11, nrow = 0))
 colnames(traitDF) <- c("time", "M1", "M2", "M3", "S", "E", "I", "R", "Date", "simulation_number", "Site")
 traitFileName <- "Concatenated_Data/model_simulations/SEI-SEIR_simulations_with_trait_variation.csv"
 write.csv(traitDF, traitFileName, row.names = F)
-
 
 for (j in 1:nrow(trait_posterior)){
   for (k in 2:ncol(trait_posterior)){
@@ -38,27 +23,33 @@ for (j in 1:nrow(trait_posterior)){
     assign(x,y)
   }
   for (l in 1:length(sites)){
-    siteclimatevars <- names(climateData)[grep(sites[l], names(climateData))]
-    climateData2 <- climateData[,c("Date", siteclimatevars)]
+    climateData2 <- subset(climateData, Site == sites[l])
+    climateData2 <- climateData2[order(climateData2$Date),]
     climateData2 <- climateData2[complete.cases(climateData2),]
-    temp <- climateData2[, grep('temp', names(climateData2))]
-    hum <- climateData2[, grep('humidity', names(climateData2))]
-    rain <- climateData2[, grep('cumRain', names(climateData2))]
-    M0 <- K_thr(temp[1], hum[1], rain[1])
+    temp <- climateData2$Temperature
+    rain <- climateData2$Two_week_rainfall
+    Rmax <- 123
+    if (unique(climateData2$country) == "Ecuador"){
+      K_trh <- K_trh_quadratic
+    } else {
+      K_trh <- K_trh_briere
+    }
+    hum <- climateData2$SVPD
     Date <- climateData2$Date
     H0 <- population[l]
     city <- sites[l]
+    BR <- BRs[l]
+    DR <- DRs[l]
     times <- seq(1,length(Date), by=1)
-    parameters <- c(EFD, pEA, MDR, K_thr, a, pMI, mu_th, PDR, b, timestep=timestep)
+    M0 <- K_trh(temp[1], hum[1], mean(rain), Rmax, H0)
+    parameters <- c(EFD, pEA, MDR, K_trh, a, pMI, mu_th, PDR, b, timestep=timestep)
     state <- c(M1 = startIC$m1*M0, M2 = startIC$m2*M0, M3 = startIC$m3*M0, S = startIC$s*H0, E = startIC$e*H0, I = startIC$i*H0, R = startIC$r*H0)
     out <- ode(y = state, times = times, func = seiseir_model_thr, parms = parameters, method="rk4", atol = 1e-14, rtol = 1e-14, hini = timestep)
     out2 <- as.data.frame(out)
     out2$Date <- Date
     out2$simulation_number <- j
     out2$Site <- sites[l]
-    traitDF <- rbind(traitDF, out2)
-    write.csv(traitDF, traitFileName, row.names = F)
+    write.table(out2, file=traitFileName, row.names = F, sep = ",", col.names = !file.exists(traitFileName), append = T)
     cat("finished running ode for", sites[l], "simulation #", j, "\n")
   }
 }
-
