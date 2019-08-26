70491ec004ce33cb72e3ff28f44886feac5e4ecb
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2019-08-26 16:20:06 +1000

    Update merged data with secondary adult dataset from Kenya.

--- Codes/merge_vector_data.R
+++ Codes/merge_vector_data.R
@@ -1,8 +1,12 @@
 # merge vector data --------------------------------------------------
 rm(list=ls()) #remove previous variable assignments
 
+# load library
+library(purrr)
+
 # load vector data
 adults_kenya <- read.csv("Concatenated_Data/vector_data/Kenya_prokopack.csv", head=T, stringsAsFactors = F)
+adults_kenya_total <- read.csv("Concatenated_Data/vector_data/Kenya_prokopack2.csv", head=T, stringsAsFactors = F)
 adults_kenya_bg <- read.csv("Concatenated_Data/vector_data/Kenya_bg.csv", head=T, stringsAsFactors = F)
 adults_ecuador <- read.csv("Concatenated_Data/vector_data/vector_data_Ecuador.csv", head=T, stringsAsFactors = F)
 larvae <- read.csv("Concatenated_Data/vector_data/Kenya_larvae.csv", head=T, stringsAsFactors = F)
@@ -10,7 +14,13 @@ eggs <- read.csv("Concatenated_Data/vector_data/Kenya_ovitrap.csv", head=T, stri
 
 # combine vector data
 vectors <- rbind(adults_kenya[,c("Site", "Date", "Year.Month", "aedes_total")], adults_ecuador[,c("Site", "Date",  "Year.Month","aedes_total")])
-vectors <- Reduce(function(x, y) merge(x, y, by=c("Site", "Date", "Year.Month"), all=T), list(vectors, adults_kenya_bg, larvae, eggs))
+vectors <- Reduce(function(x, y) merge(x, y, by=c("Site", "Date", "Year.Month"), all=T), list(vectors, adults_kenya_total, adults_kenya_bg, larvae, eggs))
+
+# remove year-month variable
+vectors$Year.Month <- NULL
+
+# format date
+vectors$Date <- as.Date(vectors$Date, "%Y-%m-%d")
 
 # save data
-write.csv(vectors, "Concatenated_Data/vector_data/merged_vector_data.csv", row.names = F)
+save(vectors, file="Concatenated_Data/vector_data/merged_vector_data.RData", row.names = F)
